59af1fe60bcc0d9813330a73afb693aac337915c
SVARUN06 <51311199+SVARUN06@users.noreply.github.com>
2019-06-12 12:37:16 +1000

    Automation scripts
    Uploaded by Arun

---
+++ Scripts/ConfigAudit_Dtemplate_Rdevice.py
@@ -0,0 +1,77 @@
+import difflib,filecmp
+
+golden_folder = "C:\\Users\\svaru\\OneDrive\\Desktop\\Goldenconfig\\Golden\\"
+config_folder = "C:\\Users\\svaru\\OneDrive\\Desktop\\Goldenconfig\\Version\\"
+report_folder = "C:\\Users\\svaru\\OneDrive\\Desktop\\Goldenconfig\\report\\"
+
+fname1 = golden_folder + "baseconfig.txt"
+fname2 = config_folder + "new template for 9010.txt"
+
+print fname1
+print fname2
+
+f1 = open(fname1)
+f2 = open(fname2)
+
+lines1 = f1.readlines()
+lines2 = f2.readlines()
+
+if not filecmp.cmp(fname1, fname2):
+        print("Golden Template Vs Current Template are not equal")
+        difference = difflib.HtmlDiff().make_file(lines1,lines2,f1,f2)
+        difference_report = open('C:\\Users\\svaru\\OneDrive\\Desktop\\Goldenconfig\\report\\Audit_diff_template_2.html','w')
+        difference_report.write(difference)
+        difference_report.close()
+        diff2 = difflib.context_diff(lines1,lines2)
+        print("Cisco:Base_template Vs new_template Difference_HTML generated successfully")
+        diff2_report = open('C:\\Users\\svaru\\OneDrive\\Desktop\\Goldenconfig\\report\\Audit_diff_template_2.csv','w')
+        diff2_report.writelines(diff2)
+        diff2_report.close()
+        f1.close()
+        f2.close()
+        print("Cisco:Base_template Vs new_template Difference_CSV generated successfully")
+
+
+#Run if templates are equal       
+'''else:
+    print("Both are equal")
+    entry_file = golden_folder + "S1CW-E-709_A.txt"
+    exit_file = config_folder + "S1CW-E-709_B.txt"
+    entry_file_lines = open(entry_file,'r').readlines()
+    exit_file_lines = open(exit_file,'r').readlines()
+    audit_diff = difflib.HtmlDiff().make_file(entry_file_lines,exit_file_lines,entry_file,exit_file)
+    print("Audit file Generating.....")
+    difference_audit = open('C:\\Users\\svaru\\OneDrive\\Desktop\\Goldenconfig\\report\\audit_diff_device.html','w')
+    difference_audit.write(audit_diff)
+    difference_audit.close()
+    print("ASR:Entry_Config Vs Exit_config Diff_Report generated successfully")'''
+    
+#Run irresepective of the template comparision
+print("Golden Template Vs new template are not equal: Device config are compared here")
+entry_file = golden_folder + "S1CW-E-709_A.txt"
+exit_file = config_folder + "S1CW-E-709_B.txt"
+print entry_file
+print exit_file
+print("Audit file HTML Generating.....0%")
+entry_file_lines = open(entry_file,'r').readlines()
+exit_file_lines = open(exit_file,'r').readlines()
+audit_diff = difflib.HtmlDiff().make_file(entry_file_lines,exit_file_lines,entry_file,exit_file)
+print("Audit file HTML Generating.....80%")
+difference_audit = open('C:\\Users\\svaru\\OneDrive\\Desktop\\Goldenconfig\\report\\audit_diff_device_2.html','w')
+difference_audit.write(audit_diff)
+print("Config Audit file HTML Generating 100%")
+difference_audit.close()
+diff3 = difflib.context_diff(entry_file_lines,exit_file_lines)
+print("Cisco:Golden_device_config Vs running_config Difference_HTML generated successfully")
+print("Audit difference CSV Generating.....0%")
+diff3_report = open('C:\\Users\\svaru\\OneDrive\\Desktop\\Goldenconfig\\report\\Audit_diff_running_2.csv','w')
+diff3_report.writelines(diff3)
+print("Audit difference CSV Generated.....100%")
+diff3_report.close()
+print("ASR:Entry_Config Vs Exit_config Diff_Report CSV generated successfully")
+
+    
+
+    
+
+
